52833d4851e89043c9baa434e9bcc4bf206a710b
Hidden Z <hiddenz@mail.i2p>
2016-12-21 19:07:49 +0000

    Make nginx dependency versioned

--- metadata.rb
+++ metadata.rb
@@ -6,4 +6,4 @@ description      'Installs/Configures nginx-i2p'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-depends "nginx"
+depends "nginx", '=2.7.6'
